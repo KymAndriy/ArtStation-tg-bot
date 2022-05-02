a31e3c65545fb241f3af4f769fcab7f42403662c
Andrii Mykhash <andrii.mykhash@globallogic.com>
2022-05-02 16:35:16 +0300

    Need to fix Logic Error

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,13 @@
 #include <exception>
 #include <string>
 #include <vector>
-
+#include <algorithm>
 #include <tgbot/tgbot.h>
 
 #include "JsonParser.h"
 #include "UrlGrabber.h"
-// #include 
+#include <boost/algorithm/string.hpp>
+// #include
 
 using namespace TgBot;
 
@@ -23,12 +24,16 @@ void createKeyboard(const std::map<std::string, std::string> &buttonStringsMap, 
     int i = 0;
     for (auto it = buttonStringsMap.begin(); it != buttonStringsMap.end(); it++)
     {
+        std::string button_text = it->first;// = boost::algorithm::to_lower_copy(it->first);
+        std::string button_com = boost::algorithm::to_lower_copy(button_text);
+        std::replace( button_com.begin(), button_com.end(), '_', ' ');
         InlineKeyboardButton::Ptr checkButton(new InlineKeyboardButton);
-        checkButton->text = it->first;
-        checkButton->callbackData = it->first;
+        checkButton->text = button_text;
+        // checkButton->
+        checkButton->callbackData = button_com;
         row.push_back(checkButton);
 
-        if((i % 4) == 0)
+        if ((i % 3) == 0)
         {
             keyboard->inlineKeyboard.push_back(row);
             row.clear();
@@ -37,16 +42,23 @@ void createKeyboard(const std::map<std::string, std::string> &buttonStringsMap, 
     }
 }
 
-void createComands(const std::map<std::string, std::string> &buttonStringsMap, Bot& bot, InlineKeyboardMarkup::Ptr &keyboard)
+void createComands(const std::map<std::string, std::string> &buttonStringsMap, Bot &bot, InlineKeyboardMarkup::Ptr &keyboard)
 {
+    // int i = 0;
+
     for (auto it = buttonStringsMap.begin(); it != buttonStringsMap.end(); it++)
     {
-        // bot.getEvents().onCallbackQuery([&bot, &keyboard, &it](CallbackQuery::Ptr query) {
-            // if (StringTools::startsWith(query->data, it->first)) {
-        bot.getEvents().onCommand(it->first, [&bot, &keyboard, &it](Message::Ptr message) {
-            // bot.getApi().sendMessage(query->message->chat->id, it->second);//, false, 0, keyboard, "Markdown");
-            bot.getApi().sendMessage(message->chat->id, it->second);//, false, 0, keyboard, "Markdown");
-            // }
+        std::string com = boost::algorithm::to_lower_copy(it->first);
+        std::replace( com.begin(), com.end(), ' ', '_');
+        // com.append(std::to_string(i++));
+
+        bot.getEvents().onCommand(com, [&bot, &keyboard, it](Message::Ptr message)
+                                  { bot.getApi().sendMessage(message->chat->id, it->second, false, 0, keyboard, "Markdown"); });
+        
+        bot.getEvents().onCallbackQuery([&bot, &keyboard,com, it](CallbackQuery::Ptr query) {
+            if (StringTools::startsWith(query->data,com)) {
+                bot.getApi().sendMessage(query->message->chat->id, it->second, false, 0, keyboard, "Markdown");
+            }
         });
     }
 }
@@ -55,7 +67,7 @@ int main()
 {
     JsonParser p("/home/andrii/temp_project/ArtStation-tg-bot/bot_configs.json");
     p.parse();
-    
+
     // std::string str;
     // std::fstream fst("/home/andrii/temp_project/ArtStation-tg-bot/artwork.json", std::ios::in);
     // std::string temp;
@@ -63,7 +75,7 @@ int main()
     // {
     //     fst >> temp;
     //     str.append(temp);
-    // // } 
+    // // }
     // UrlGrabber grab;//(p.getImageUrlRegex(), str);
     // grab.setParseStr(str);
     // grab.setRegexExpression(p.getImageRegex());
@@ -71,17 +83,32 @@ int main()
 
     Bot bot(p.getToken());
 
-
+    std::map<std::string, std::string> buttonStringsMap = p.getNameAndUrls();
     // Thanks Pietro Falessi for code
     InlineKeyboardMarkup::Ptr keyboard(new InlineKeyboardMarkup);
-    createKeyboard(p.getNameAndUrls(),keyboard);
+    createKeyboard(p.getNameAndUrls(), keyboard);
 
+    // createComands(p.getNameAndUrls(), bot, keyboard);
+
+    // int i = 0;
+    for (auto it = buttonStringsMap.begin(); it != buttonStringsMap.end(); it++)
+    {
+        std::string com = boost::algorithm::to_lower_copy(it->first);
+        std::replace( com.begin(), com.end(), ' ', '_');
+        bot.getEvents().onCommand(com, [&bot, it](Message::Ptr message) {
+            bot.getApi().sendMessage(message->chat->id, it->second);//, false, 0, , "Markdown");
+        });
+
+        //  bot.getEvents().onCallbackQuery([&bot, &keyboard,com, it](CallbackQuery::Ptr query) {
+        //     if (StringTools::startsWith(query->data, com)) {
+        //         bot.getApi().sendMessage(query->message->chat->id, it->second);//, false, 0, 0, "Markdown");
+        //     }
+        // });
+    }
     bot.getEvents().onCommand("start", [&bot, &keyboard](Message::Ptr message) {
-        bot.getApi().sendMessage(message->chat->id, "Hi!", false, 0, keyboard);
-        // bot.getApi().
+        bot.getApi().sendMessage(message->chat->id, 0, false, 0, keyboard);
     });
-    createComands(p.getNameAndUrls(), bot, keyboard);
-   
+
     // bot.getEvents().onCallbackQuery([&bot, &keyboard](CallbackQuery::Ptr query) {
     //     if (StringTools::startsWith(query->data, "check")) {
     //         std::string response = "ok";
@@ -89,36 +116,35 @@ int main()
     //     }
     // });
 
-
-    signal(SIGINT, [](int s) {
+    signal(SIGINT, [](int s)
+           {
         printf("SIGINT got\n");
-        exit(0);
-    });
+        exit(0); });
 
-    try {
-        printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
-        bot.getApi().deleteWebhook();
+    // try {
+    printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
+    bot.getApi().deleteWebhook();
 
-        TgLongPoll longPoll(bot);
-        while (true) {
-            printf("Long poll started\n");
-            longPoll.start();
-        }
-    } catch (std::exception& e) {
-        printf("error: %s\n", e.what());
+    TgLongPoll longPoll(bot);
+    while (true)
+    {
+        printf("Long poll started\n");
+        longPoll.start();
     }
-// 
+    // } catch (std::exception& e) {
+    //     printf("error: %s\n", e.what());
+    // }
+    //
     // return 0;
 
     // for(size_t i = 0; i < _urls.size(); i++)
     // {
     //     std::cout << _urls[i] << std::endl;
     // }
-    
+
     // fst.close();
     return 0;
 }
-
 
 /*
     #include <csignal>
